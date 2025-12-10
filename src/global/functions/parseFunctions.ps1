8f658745bf92ac7221e314f61a989780a3a9324a
Jefrey <hernandezrodriguezjefrey@gmail.com>
2025-12-10 13:58:17 +0100

    12/10/25 - Initial commit to start coding

---
+++ src/global/functions/parseFunctions.ps1
@@ -0,0 +1,37 @@
+function Parse-JsonElements {
+    param(
+        [Parameter(Mandatory)]
+        $Elements
+    )
+
+    $elementObjects = [System.Collections.Generic.List[object]]::new()
+    $current = $null
+
+    foreach ($element in $Elements) {
+
+        $name  = $element.Name
+        $value = $element.Value
+
+        if ($name -eq "filename") {
+
+            if ($current) {
+                $elementObjects.Add($current)
+            }
+
+            $current = [PSCustomObject]@{
+                script = $value
+            }
+            continue
+        }
+
+        $property = ($name -split "\.")[-1]
+
+        $current | Add-Member -NotePropertyName $property -NotePropertyValue $value -Force
+    }
+
+    if ($current) {
+        $elementObjects.Add($current)
+    }
+
+    return $elementObjects
+}
---
+++ src/paths.ps1
@@ -0,0 +1,8 @@
+$rootFilepath = $PSScriptRoot
+
+$assetsFolderpath = "$rootFilepath\assets"
+$clientFolderpath = "$rootFilepath\client"
+$globalFolderpath = "$rootFilepath\global"
+$serverFolderpath = "$rootFilepath\server"
+
+$serverKeysFolderpath = "$serverFolderpath\keys"
