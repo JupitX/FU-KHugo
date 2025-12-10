3e3e37a191a67be4579e0a2ef5a6614b5530979a
Jefrey <hernandezrodriguezjefrey@gmail.com>
2025-12-10 18:10:58 +0100

    12/10/25 - file moved to scripts folder

---
+++ src/global/scripts/keys.ps1
@@ -0,0 +1,29 @@
+. (Resolve-Path "..\..\paths.ps1")
+
+$key = New-Object byte[] 32
+$iv = New-Object byte[] 16
+$id = [guid]::NewGuid().ToString()
+
+$keysFilepath = "$keysFolderpath\$id.json"
+
+[System.Security.Cryptography.RandomNumberGenerator]::Create().GetBytes($key)
+[System.Security.Cryptography.RandomNumberGenerator]::Create().GetBytes($iv)
+
+$keyHex = ""
+ForEach ($byte in $key) {
+    $keyHex += $byte.ToString("X2")
+}
+
+$ivhex = ""
+ForEach ($byte in $iv) {
+    $ivhex += $byte.ToString("X2")
+}
+
+$keys = [PSCustomObject]@{
+	Key = $keyHex
+	IV = $ivhex
+}
+
+$keysJSON = ConvertTo-Json $keys
+
+Set-Content -Path $keysFilepath -Value $keysJSON -Encoding UTF8
