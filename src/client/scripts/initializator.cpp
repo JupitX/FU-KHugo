191d125c3eb04b6562511e3fe72d008e5f3d7e4f
Jefrey <hernandezrodriguezjefrey@gmail.com>
2025-12-15 03:19:40 +0100

    12/15/25 - Initializer server request functionality added

--- src/client/scripts/initializator.cpp
+++ src/client/scripts/initializator.cpp
@@ -2,6 +2,7 @@
 #include "..\..\global\scripts\headers\deobfuscator.hpp"
 
 #include <iostream>
+#include <curl/curl.h>
 
 int main() {
     const std::vector<uint8_t> OBFUSCATED = data;
@@ -12,35 +13,17 @@ int main() {
     const unsigned int MULTIPLIER = multiplier;
 
     std::string licenseServer = deobfuscate(OBFUSCATED, KEYS, MASTERKEY, shift, ROTATION_BITS, MULTIPLIER);
-    std::cout << "IP: " << licenseServer << "\n";
+    std::string url = "https://" + licenseServer + ":443";
 
-    std::cout << "------------------------------\n";
-    std::cout << "--- OBFUSCATION PARAMETERS ---\n";
-    std::cout << "------------------------------\n\n";
-    std::cout << "LFSR_SEED: " << std::hex << shift.seed << "\n";
-    std::cout << "LFSR_TAP:  " << std::hex << shift.tap << "\n";
-    std::cout << "ROTATION_BITS: " << ROTATION_BITS << "\n";
-    std::cout << "MULTIPLIER: " << MULTIPLIER << "\n";
+    std::cout << url;
 
-    std::cout << "------------------------------\n";
-    std::cout << "------- INTEGRITY KEYS -------\n";
-    std::cout << "------------------------------\n\n";
-    std::cout << "KEYS: ";
+    CURL* curl = curl_easy_init();
 
-    for (size_t i = 0; i < keys.size(); i++) {
-        std::cout << std::hex << keys[i] << ",";
-    }
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
 
-    std::cout << "\n\n";
-
-    std::cout << "------------------------------\n";
-    std::cout << "--------- MASTER KEY ---------\n";
-    std::cout << "------------------------------\n\n";
-    std::cout << "Master Key: " << std::hex << masterKey << "\n";
-
-    std::cout << "------------------------------\n";
-    std::cout << "------ DEOBFUCATED DATA ------\n";
-    std::cout << "------------------------------\n\n";
-    std::cout << "CLEARTEXT: " << licenseServer << "\n";
+    return 0;
 
 }
