32bd3c42fcec781d403625dd17d29aa0e3101ca4
Jefrey <hernandezrodriguezjefrey@gmail.com>
2025-12-15 18:24:02 +0100

    12/15/25 - Initialization of General application layout definition

---
+++ src/server/c2c/frontend/src/layout/AppLayout.tsx
@@ -0,0 +1,2 @@
+import { SidebarProvider, useSidebar } from "../context/SidebarContext";
+import { Outlet } from "react-router";
---
+++ src/server/c2c/frontend/src/pages/Dashboard/Home.tsx
@@ -0,0 +1,15 @@
+import Metadata from "../../components/common/Metadata";
+import StatisticsChart from "../../components/metrics/StatisticsChart";
+
+export default function Home() {
+    return (
+        <>
+            <Metadata
+                title = "Fuckhugo C2C - Home"
+                description = "This is the home page of the Fuckhugo All-In-One RAT tool"
+            />
+
+            
+        </>
+    );
+}
