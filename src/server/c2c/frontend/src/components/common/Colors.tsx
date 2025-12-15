fc6f93b9a689d74af12304cbf61fe7fa79c817b1
Jefrey <hernandezrodriguezjefrey@gmail.com>
2025-12-15 18:08:29 +0100

    12/15/25 - Definition of color palette for the website

---
+++ src/server/c2c/frontend/src/components/common/Colors.tsx
@@ -0,0 +1,10 @@
+export const Palette = {
+    light: {
+        electricBlue: "#465FFF",
+        lightBlue: "#9CB9FF",
+        background: "#fff",
+        lightGray: "#6B7280"
+    }
+}
+
+export default Palette;
