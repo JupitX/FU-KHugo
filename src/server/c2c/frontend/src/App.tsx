d451f548be7177c8067f1f91f37080287bb634c2
Jefrey <hernandezrodriguezjefrey@gmail.com>
2025-12-16 03:29:13 +0100

    12/16/25 - Main code modified to display current file in development

--- src/server/c2c/frontend/src/App.tsx
+++ src/server/c2c/frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import StatisticsChart from "./components/metrics/StatisticsChart";
 import { ScrollToTop } from "./components/common/ScrollToTop";
+import NotificationDropdown from "./components/header/NotificationDropdown";
+import UserDropdown from "./components/header/UserDropdown";
+import { ThemeToggler } from "./components/common/ThemeToggler";
+import AppHeader from "./layout/AppHeader"
 
 function App() {
   return (
@@ -8,9 +12,7 @@ function App() {
       <Router>
         <ScrollToTop/>
         <Routes>
-          <Route element={}>
-
-          </Route>
+          <Route index path="/" element={<AppHeader/>}/>
         </Routes>
       </Router>
     </>
