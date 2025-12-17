db7b9b2b4a7c5246c4095bba7bb8777d22b85270
Jefrey <hernandezrodriguezjefrey@gmail.com>
2025-12-17 01:16:24 +0100

    12/17/25 - New light colors added

--- src/server/c2c/frontend/src/components/common/Colors.tsx
+++ src/server/c2c/frontend/src/components/common/Colors.tsx
@@ -3,7 +3,11 @@ export const Palette = {
         electricBlue: "#465FFF",
         lightBlue: "#9CB9FF",
         background: "#fff",
-        lightGray: "#6B7280"
+        lightGray: "#6B7280",
+        beige: "#E4E7EC",
+        darkgray: "#1D2939",
+        red: "#D92D20",
+        green: "#039855"
     }
 }
 
