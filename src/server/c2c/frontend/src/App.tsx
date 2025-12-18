00fcb0a8a8da40ae561abeecf1e5c06b0a90dc1d
Jefrey <hernandezrodriguezjefrey@gmail.com>
2025-12-18 02:04:49 +0100

    12/18/25 - Deletion of unnecessary imports

--- src/server/c2c/frontend/src/App.tsx
+++ src/server/c2c/frontend/src/App.tsx
@@ -1,14 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
-import StatisticsChart from "./components/metrics/StatisticsChart";
 import { ScrollToTop } from "./components/common/ScrollToTop";
-import NotificationDropdown from "./components/header/NotificationDropdown";
-import UserDropdown from "./components/header/UserDropdown";
-import { ThemeToggler } from "./components/common/ThemeToggler";
-import AppHeader from "./layout/AppHeader";
-import Sidebar from "./layout/Sidebar";
 import AppLayout from "./layout/AppLayout";
 import Home from "./pages/Dashboard/Home";
-import MonthlyConnectionsChart from "./components/metrics/MonthlyConnectionsChart";
 
 function App() {
   return (
